d75ee396b1ebfcfc83e6b739158ef745aa8818e7
Lily Vang <lily.sf888@gmail.com>
2017-01-25 09:41:26 -0800

    fixed header

---
+++ fixes.js
@@ -0,0 +1,29 @@
+//work on name and title styling
+
+//set breakpoint at 1100px - Jesse
+
+//describe what each project does above links in whitespace
+
+//make projects open in new window
+
+//fix Readme for zumba and chakras - Jesse
+
+//Zumba fixes:
+
+//set up demo account for login page e.g. please use demo@demo.com to try out the apps
+
+//make "Get your zumba on" much smaller
+
+//move previous and next buttons
+
+//tone down the background
+
+//chakra fixes:
+
+//my chakras main page needs some guidance on what to do
+
+//edit content on the chakras
+
+//put feedback into some kind of log for the user - Jesse
+
+//youtube api links for yoga stretches
