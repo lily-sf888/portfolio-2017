1eac140b9bd1b2c58bbc3afda6b5507516e58853
Lily Vang <lily.sf888@gmail.com>
2017-01-26 15:33:45 -0800

    editing project section

--- fixes.js
+++ fixes.js
@@ -1,17 +1,10 @@
 
-//fix Readme for zumba and chakras - Jesse
+//fix Readme for zumba and chakras
 
 // all heroku apps: https://quickleft.com/blog/6-easy-ways-to-prevent-your-heroku-node-app-from-sleeping/
 
 //Zumba fixes:
 
-//set up demo account for login page e.g. please use demo@demo.com to try out the apps
-
-//make "Get your zumba on" much smaller (Lily)
-
-//move previous and next buttons (Lily)
-
-//tone down the background (Lily)
 
 //chakra fixes:
 
